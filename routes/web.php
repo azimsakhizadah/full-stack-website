101af942c137490bd94599f62f9f231cdc85d32c
azimchamran <m.azims1396@gmail.com>
2025-11-15 11:42:33 +0330

    login form created

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
@@ -19,4 +20,6 @@ Route::get('/courses', function () {
     return view('courses');
 });
 
-Route::get();
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+
+Route::post('/login', [LoginController::class, 'login'])->name('login.post');
